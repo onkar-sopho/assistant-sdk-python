c779de8189248b8bbc0cbf4f254ae5872cad3b6f
Onkar <onkar21.rai@gmail.com>
2019-05-05 23:18:30 +0530

    added On/Off trait

--- google-assistant-sdk/googlesamples/assistant/library/hotword.py
+++ google-assistant-sdk/googlesamples/assistant/library/hotword.py
@@ -66,6 +66,11 @@ def process_event(event):
     if event.type == EventType.ON_DEVICE_ACTION:
         for command, params in event.actions:
             print('Do command', command, 'with params', str(params))
+            if command == "action.devices.commands.OnOff":
+                if params['on']:
+                    print('Turning the LED on.')
+                else:
+                    print('Turning the LED off.')
 
 
 def main():
